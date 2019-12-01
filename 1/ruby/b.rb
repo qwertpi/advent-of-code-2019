6c6f5d3509cee2e5165bf665c0de785fd8931a5b
qwertpi <qwertpi@users.noreply.github.com>
2019-12-01 19:56:54 +0000

    Solved 1b

---
+++ 1/ruby/b.rb
@@ -0,0 +1,20 @@
+def fuel_calc(mass)
+    #string to int
+    mass = mass.to_i
+    #divison between ints is floor by deafult
+    fuel = mass / 3
+    fuel -= 2
+    return fuel
+end
+
+total_fuel = 0
+File.open("input.txt", "r").each_line do |line|
+    new_fuel = fuel_calc(line)
+    while new_fuel > 0
+        total_fuel += new_fuel
+        new_fuel = fuel_calc(new_fuel)
+    end
+#closes file
+end
+#outputs answer
+puts(total_fuel)
