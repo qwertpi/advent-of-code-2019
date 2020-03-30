82442552c006c4bbcaf31b106df22ea9979aef26
qwertpi <qwertpi@users.noreply.github.com>
2020-03-30 13:17:24 +0100

    Solved 1b using scala

---
+++ 1/scala/b.scala
@@ -0,0 +1,15 @@
+import math.floor
+import io.Source.fromFile
+def fuel_calc(mass: Int): Int ={
+    return floor(mass/3).toInt - 2
+}
+
+var total_fuel : Int = 0
+for (line <- fromFile("input.txt").getLines){
+    var new_fuel: Int = fuel_calc(line.toInt)
+    while (new_fuel >= 0) {
+        total_fuel += new_fuel
+        new_fuel = fuel_calc(new_fuel)
+    }
+}
+println(total_fuel)
