b5c6c9124f6368df1be05b24bef5cf6a3dfe049e
qwertpi <qwertpi@users.noreply.github.com>
2020-05-04 14:13:08 +0100

    Made all code compilable with scalac by wrapping in an 'object extends App'

--- 1/scala/a.scala
+++ 1/scala/a.scala
@@ -1,12 +1,13 @@
 import math.floor
 import io.Source.fromFile
-
-def fuel_calc(mass: Int): Int ={
-    return floor(mass/3).toInt - 2
+object a extends App {
+    def fuel_calc(mass: Int): Int ={
+        return floor(mass/3).toInt - 2
+    }
+    
+    var total_fuel: Int = 0
+    for (line <- fromFile("input.txt").getLines){
+        total_fuel += fuel_calc(line.toInt)
+    }
+    println(total_fuel)
 }
-
-var total_fuel: Int = 0
-for (line <- fromFile("input.txt").getLines){
-    total_fuel += fuel_calc(line.toInt)
-}
-println(total_fuel)
